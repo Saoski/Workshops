e951a1461bcb3c9ebeb8f2728479a4c4c43bb7c9
saoski <saoski88@gmail.com>
2024-09-29 10:54:37 -0400

    Complete workshop 2

---
+++ workshop-02-typescript/practice-problems/problem-1/main.js
@@ -0,0 +1,12 @@
+// Fill in for exercises!
+function main() {
+  var userName = "Sean";
+  var favoriteFoods = ["chicken alfredo", "cheese its", "ramen"];
+  var user = {
+    name: userName,
+    age: 18,
+    favoriteFoods: favoriteFoods,
+  };
+  console.log(user);
+}
+main();
--- workshop-02-typescript/practice-problems/problem-1/main.ts
+++ workshop-02-typescript/practice-problems/problem-1/main.ts
@@ -1,1 +1,14 @@
 // Fill in for exercises!
+function main() {
+  let userName: string = "Sean";
+  let favoriteFoods: string[] = ["chicken alfredo", "cheese its", "ramen"];
+  let user: { name: string; age: number; favoriteFoods: string[] } = {
+    name: userName,
+    age: 18,
+    favoriteFoods: favoriteFoods,
+  };
+
+  console.log(user);
+}
+
+main();
